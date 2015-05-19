1b342cbc6919da1e8144a5ee2d7aff80425166b0
Rotem Rubnov <rotem@100grams.nl>
2015-05-19 22:27:46 +0200

    added checks for optional delegate methods

--- JASwipeCell/JASwipeCell.m
+++ JASwipeCell/JASwipeCell.m
@@ -441,7 +441,9 @@ typedef NS_ENUM(NSUInteger, JASwipeDirection) {
                     [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
                         [self pinButtonToTopViewWithOffset:newXOffset swipeDirection:JASwipeDirectionLeft];
                     } completion:^(BOOL finished) {
-                        [self.delegate rightMostButtonSwipeCompleted:self];
+                        if ([self.delegate respondsToSelector:@selector(rightMostButtonSwipeCompleted:)]) {
+                            [self.delegate rightMostButtonSwipeCompleted:self];
+                        }
                     }];
                     self.rightButtonsRevealed = NO;
                 }
@@ -478,7 +480,9 @@ typedef NS_ENUM(NSUInteger, JASwipeDirection) {
                     [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
                         [self pinButtonToTopViewWithOffset:newXOffset swipeDirection:JASwipeDirectionRight];
                     } completion:^(BOOL finished) {
-                        [self.delegate leftMostButtonSwipeCompleted:self];
+                        if ([self.delegate respondsToSelector:@selector(leftMostButtonSwipeCompleted:)]) {
+                            [self.delegate leftMostButtonSwipeCompleted:self];
+                        }
                     }];
                     self.leftButtonsRevealed = NO;
                 }
